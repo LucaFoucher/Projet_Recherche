d2ca1351583446fd7b3c0d5d71213f931cb30719
LucaFoucher <USERMAILfoucherluca@gmail.com>
2023-11-13 19:53:01 +0100

    Courbes legendes

--- AFICS-main/courbes.py
+++ AFICS-main/courbes.py
@@ -1,25 +1,28 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.interpolate import CubicSpline
+from scipy.optimize import minimize_scalar
+from scipy.interpolate import UnivariateSpline
 
 #glft-psyNx5JvEF31vQBy16aK
 
-rdf = pd.read_csv('C:/Users/fouch/OneDrive/Documents/Cours_5A/AFICS-main/AFICS-main/Cr_rdf.csv')
+#rdf = pd.read_csv('C:/Users/fouch/OneDrive/Documents/Cours_5A/AFICS-main/AFICS-main/Cr_rdf.csv')
 
-rdfint = pd.read_csv('C:/Users/fouch/OneDrive/Documents/Cours_5A/AFICS-main/AFICS-main/Cr_rdfint.csv')
+#rdfint = pd.read_csv('C:/Users/fouch/OneDrive/Documents/Cours_5A/AFICS-main/AFICS-main/Cr_rdfint.csv')
 
-adf = pd.read_csv('C:/Users/fouch/OneDrive/Documents/Cours_5A/AFICS-main/AFICS-main/Cr_adf.csv')
+#adf = pd.read_csv('C:/Users/fouch/OneDrive/Documents/Cours_5A/AFICS-main/AFICS-main/Cr_adf.csv')
 
-rmsd = pd.read_csv('C:/Users/fouch/OneDrive/Documents/Cours_5A/AFICS-main/AFICS-main/Cr_RMSDs.csv')
+#rmsd = pd.read_csv('C:/Users/fouch/OneDrive/Documents/Cours_5A/AFICS-main/AFICS-main/Cr_RMSDs.csv')
 
 
-#rdf = pd.read_csv('C:/Users/fouch/OneDrive/Documents/Cours_5A/AFICS-main/AFICS-main/EDTA-rdf.csv')
+rdf = pd.read_csv('C:/Users/fouch/OneDrive/Documents/Cours_5A/AFICS-main/AFICS-main/EDTA-rdf.csv')
 
-#rdfint = pd.read_csv('C:/Users/fouch/OneDrive/Documents/Cours_5A/AFICS-main/AFICS-main/EDTA-rdf-integral.csv')
+rdfint = pd.read_csv('C:/Users/fouch/OneDrive/Documents/Cours_5A/AFICS-main/AFICS-main/EDTA-rdf-integral.csv')
 
-#adf = pd.read_csv('C:/Users/fouch/OneDrive/Documents/Cours_5A/AFICS-main/AFICS-main/EDTA-adf.csv')
+adf = pd.read_csv('C:/Users/fouch/OneDrive/Documents/Cours_5A/AFICS-main/AFICS-main/EDTA-adf.csv')
 
-#rmsd = pd.read_csv('C:/Users/fouch/OneDrive/Documents/Cours_5A/AFICS-main/AFICS-main/EDTA-RMSDs.csv')
+rmsd = pd.read_csv('C:/Users/fouch/OneDrive/Documents/Cours_5A/AFICS-main/AFICS-main/EDTA-RMSDs.csv')
 
 
 rmsd =rmsd.drop(1)
@@ -42,21 +45,25 @@ plt.figure(1)
 plt.plot(adf['Angle'],adf['Count'])
 plt.xlabel('Angle')
 plt.ylabel('Count')
-plt.title('Courbe du nombre d apparition de chaque angle')
+plt.title('Nombre de présence de chaque angle')
 
 
 fig, ax1 = plt.subplots()
 ax2 = ax1.twinx()
-ax1.plot(rdf['Distance (A)'], rdf['Count'], 'g-', label='Courbe 1')
-ax2.plot(rdf['Distance (A)'], rdfint['Integral'], 'b-', label='Courbe 2')
+ax1.plot(rdf['Distance (A)'], rdf['Count'], 'g-', label='RDF')
+ax2.plot(rdf['Distance (A)'], rdfint['Integral'], 'b-', label='Integral')
 ax2.set_ylim(0, 12)
+ax1.set_ylabel('g(r)')
+ax2.set_ylabel('Integration')
+fig.legend(loc='center right')
 
 
 plt.figure(3)
 for i in range(rmsd.shape[1]):
-    plt.plot(x,rmsd[rmsd.columns[i]])
+    plt.plot(x,rmsd[rmsd.columns[i]],label=str(rmsd.columns[i]))
     
 plt.ylim(0, 1.0)
+plt.legend(loc='best')
 
 
 
@@ -80,6 +87,13 @@ print(distrib)
 #print(type(rmsd.iloc[0,0]))
 
 #print(rmsd[rmsd.columns[0]])
+
+
+
+   
+
+
+
 
 
 
